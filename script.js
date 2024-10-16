c847c8a3cc695391e04b83176db6aee7950b8a05
Moosest <martenpoder@gmail.com>
2024-10-16 20:01:29 +0300

    Added the site

---
+++ script.js
@@ -0,0 +1,14 @@
+const url = `http://numbersapi.com/random/math?json`;
+
+async function download() {
+    const element = document.querySelector('.number')
+    element.innerHTML = '<h1 class="wb">Tere tulemast! </h1>'
+    for (let i = 0; i < 20; i++) {
+        const response = await fetch(url)
+        const json = await response.json()
+        const number = json.number
+        const text = json.text
+        element.innerHTML += "<p class='box'>" + number + " :  <br> " + text + "</p>"
+    }
+}
+download();
